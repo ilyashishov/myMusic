ec28f158d554c26ef0e51eadbe6b57d22366ddf5
Артём Михайлов < >
2015-10-23 01:10:55 +0300

    Добавил функцию для работы с группой файлов

--- codegenerator_js/index.js
+++ codegenerator_js/index.js
@@ -1,12 +1,66 @@
 var codegen = require('echoprint-codegen');
- 
-var opts = {
-  file: 'test4_1.mp3',
-  index: 10,
-  offset: 30
-}
- 
-codegen(opts, function (err, data) {
-  if (err) return console.error(err);
-  console.log(data);
+var fs = require('fs');
+
+fs.readdir( process.argv[2], function (err, files) { 
+	if (!err) {
+		files.forEach(function(item, i, arr) {
+			if(item.slice(-3) == 'mp3'){
+				var opts = {
+					  file: item,
+					  index: 10,
+					  offset: 30
+					}
+				codegen(opts, function (err, data) {
+					if (err) return console.error(err);
+						console.log(data);
+				});
+			}	
+		});
+	}
+	else
+		throw err; 
 });
+
+ 
+// var opts = {
+//   file: 'test4_1.mp3',
+//   index: 10,
+//   offset: 30
+// }
+ 
+// codegen(opts, function (err, data) {
+//   if (err) return console.error(err);
+//   console.log(data);
+// });
+
+
+// function loadXMLDoc() {
+//     var xmlhttp;
+
+//     if (window.XMLHttpRequest) {
+//         // code for IE7+, Firefox, Chrome, Opera, Safari
+//         xmlhttp = new XMLHttpRequest();
+//     } else {
+//         // code for IE6, IE5
+//         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+//     }
+
+//     xmlhttp.onreadystatechange = function() {
+//         if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+//            if(xmlhttp.status == 200){
+//                document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+//            }
+//            else if(xmlhttp.status == 400) {
+//               alert('There was an error 400')
+//            }
+//            else {
+//                alert('something else other than 200 was returned')
+//            }
+//         }
+//     }
+
+//     xmlhttp.open("GET", "http://52.27.204.237:37760", true);
+//     xmlhttp.send();
+// }
+
+// loadXMLDoc();
